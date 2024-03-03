d9c1d8878c2a2540905fa7d818f3804a34e4dbc1
shxrklol <shxrk@shxrk.dev>
2024-03-03 15:06:37 -0500

    added fonts, sass, ts, and 404 page

---
+++ public/ts/iconchange.ts
@@ -0,0 +1,10 @@
+var elements = document.querySelectorAll('.icons-a, .icons-b, .icons-c, .icons-d, .icons-e');
+
+function changeClasses() {
+   for (var i = 0; i < elements.length; i++) {
+       elements[i].classList.remove('icons-a', 'icons-b', 'icons-c', 'icons-d', 'icons-e');
+       elements[i].classList.add('icon-js');
+   }
+}
+
+setTimeout(changeClasses, 4675);
